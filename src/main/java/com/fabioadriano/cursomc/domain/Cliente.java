40208d04a7061f92c439540a04ecd0479670bd7f
Fábio Adriano <fbio.adriano46@gmail.com>
2020-03-26 17:04:56 -0300

    EndPoint/pedidods/{id} disponibilizado

--- src/main/java/com/fabioadriano/cursomc/domain/Cliente.java
+++ src/main/java/com/fabioadriano/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fabioadriano.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
@@ -37,6 +38,7 @@ private List<Endereco> enderecos = new ArrayList<>();
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	

f700339d21de7cbe5870db821ba5c7e1c79fb737
Fábio Adriano <fbio.adriano46@gmail.com>
2020-04-06 17:42:46 -0300

    Inserindo Pedido

--- src/main/java/com/fabioadriano/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/fabioadriano/cursomc/domain/PagamentoComBoleto.java
@@ -4,10 +4,12 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fabioadriano.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComBoleto")
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 

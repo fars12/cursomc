36c0050ec812dce594312ca7463f88d8a296fed5
Fábio Adriano <fbio.adriano46@gmail.com>
2020-06-08 14:31:12 -0300

    Implementando EmailHtml com servidor do Google

--- src/main/java/com/fabioadriano/cursomc/services/EmailService.java
+++ src/main/java/com/fabioadriano/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.fabioadriano.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.fabioadriano.cursomc.domain.Pedido;
@@ -7,7 +9,10 @@ import com.fabioadriano.cursomc.domain.Pedido;
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
-	
-	void sendEmail(SimpleMailMessage msg);
-}
 
+	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
+}

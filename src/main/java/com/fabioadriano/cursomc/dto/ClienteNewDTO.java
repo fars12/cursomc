d397ccf4a3a15b7a0a9224ff9f74582b4186117b
Fábio Adriano <fbio.adriano46@gmail.com>
2020-06-08 16:16:53 -0300

    Adicionando senha a Cliente

--- src/main/java/com/fabioadriano/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/fabioadriano/cursomc/dto/ClienteNewDTO.java
@@ -27,6 +27,9 @@ public class ClienteNewDTO implements Serializable {
 	private Integer tipo;
 	
 	@NotEmpty(message="Preenchimento obrigatÃ³rio")
+	private String senha;
+	
+	@NotEmpty(message="Preenchimento obrigatÃ³rio")
 	private String logradouro;
 
 	@NotEmpty(message="Preenchimento obrigatÃ³rio")
@@ -57,6 +60,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public String getEmail() {
